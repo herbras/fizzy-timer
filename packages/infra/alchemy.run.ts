30862f0c8f81282ef1a279f9a38f573eebf89998
Sarbeh <handoyonoterakhir@gmail.com>
2026-01-03 13:32:56 +0700

    initial commit

---
+++ packages/infra/alchemy.run.ts
@@ -0,0 +1,19 @@
+import alchemy from "alchemy";
+import { TanStackStart } from "alchemy/cloudflare";
+import { config } from "dotenv";
+
+config({ path: "./.env" });
+config({ path: "../../apps/web/.env" });
+
+const app = await alchemy("fizzy-timer");
+
+export const web = await TanStackStart("web", {
+  cwd: "../../apps/web",
+  bindings: {
+    VITE_CONVEX_URL: alchemy.env.VITE_CONVEX_URL!,
+  },
+});
+
+console.log(`Web    -> ${web.url}`);
+
+await app.finalize();
